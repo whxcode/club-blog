0a9b38ab59990460e254ecc732553524a1dfa40b
whx <35129760@qq.com>
2021-02-26 10:57:24 +0800

    优化页面 将 story 页面背景改为动态化。 引入 Vibrant.min.js 库 修改部分配置文件

--- src/views/story/index.tsx
+++ src/views/story/index.tsx
@@ -1,49 +1,75 @@
 import React, {Dispatch, memo, SetStateAction, useCallback, useEffect, useMemo, useState} from "react"
-import AwesomeSwiper,{ ReactSwiperProps } from 'react-awesome-swiper'
-import  Swiper,{ SwiperOptions } from 'swiper';
+import AwesomeSwiper, {ReactSwiperProps} from 'react-awesome-swiper'
+import Swiper, {SwiperOptions} from 'swiper';
 import './index.less'
-import {useHistory} from "react-router";
+import {useHistory} from "react-router"
+
+
+const imgs: any = []
+for (let i = 1; i <= 10; ++i) {
+    imgs.push(require(`@/asserts/images/test/${i}.jpg`))
+}
+
+// @ts-ignore
+window.onLoad = function onLoad(this: any) {
+    // @ts-ignore
+    const img = window.event.path[0]
+    // @ts-ignore
+    const root = window.event.path[2]
+    const content = root.querySelector('.story-content>.hot')
+
+    // @ts-ignore
+    var vibrant = new window.Vibrant(img);
+    var swatches = vibrant.swatches()
+    content.style.background = `${swatches.Muted.getHex()}56`
+}
+
+
+const Dynamic = memo(() => {
+    return <div className="dynamic beat">
+        <span className="iconfont iconmoreunfold"/>
+        <p>上拉查看更多精彩故事</p>
+    </div>
+})
 const Story = memo(() => {
-    // const [list,setList] = useState(new Array(5).fill(1))
-    // const [count,setCount] = useState(0)
-    const { push,goBack } = useHistory()
+    const {push, goBack} = useHistory()
     const list = useMemo(() => {
-        return [1,2,3,4,5]
-    },[])
-    const  slideChange =  useMemo( () => {
-        let off = true
-        return function (this: Swiper) {
-            if(list.length >= 10 && off) {
-                this.appendSlide(`<div class="not-data">没有数据了</div>`)
-                off = false
-                return;
-            }
+        return [1, 2, 3, 4, 5]
+    }, [])
+    const slideChange = useMemo(() => {
+            let off = true
+            return function (this: Swiper) {
+                if (list.length >= 10 && off) {
+                    this.appendSlide(`<div class="not-data">没有数据了</div>`)
+                    off = false
+                    return;
+                }
 
-            if( off &&  (list.length - this.activeIndex) <= 2  ) {
-                console.log('add')
-                const newData = [1,2,3,4,5]
-                renderList(this,newData)
-                list.push(...newData)
+                if (off && (list.length - this.activeIndex) <= 2) {
+
+                    const newData = [1, 2, 3, 4, 5]
+                    renderList(this, newData)
+                    list.push(...newData)
+
+                }
 
             }
-            // console.log(list)
         }
-    }
-    ,[])
+        , [])
 
-    const renderList = useCallback((slider,list: Array<any>) => {
-        slider.appendSlide(list.map((i,ii) => {
+    const renderList = useCallback((slider, list: Array<any>) => {
+        slider.appendSlide(list.map((i, ii) => {
             return `
                      <div class="swiper-slide story"
                                        key={ index }
                             >
                             <div class="story-bg">
-                                <img src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1608067339,398987776&fm=26&gp=0.jpg" alt=""/>
+                                <img src="${imgs[ii % 10]}" alt="" onLoad="onLoad(this)"/>
                             </div>
                             <div class="story-content">
-                                <div class="user-wrapper" onclick="_toProfile()" data-id="${ ii }" >
+                                <div class="user-wrapper" onclick="_toProfile()" data-id="${ii}" >
                                     <div class="avatar-wrapper">
-                                        <img src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3728388047,2037988101&fm=26&gp=0.jpg" alt="" className="avatar"/>
+                                        <img src="${imgs[ii % 10]}" alt="" className="avatar"/>
                                     </div>
                                     <div class="base">
                                         <h1 class="name line-1">Jasmine Levin</h1>
@@ -71,9 +97,9 @@ const Story = memo(() => {
                         </div>
                     `
         }))
-    },[])
+    }, [])
 
-    const [config,setConfig]: [SwiperOptions,Dispatch<SetStateAction<SwiperOptions>>] = useState<SwiperOptions>( {
+    const [config, setConfig]: [SwiperOptions, Dispatch<SetStateAction<SwiperOptions>>] = useState<SwiperOptions>({
         direction: 'vertical',
         slidesPerView: 1,
         centeredSlides: true,
@@ -82,67 +108,33 @@ const Story = memo(() => {
         on: {
             slideChange,
             init(w) {
-              renderList(this,list)
+                renderList(this, list)
             }
         },
     })
 
 
     useEffect(() => {
-        setConfig({ ...config,height: window.screen.height })
+        setConfig({...config, height: window.screen.height})
         // @ts-ignore
         window._toProfile = (e) => {
             // @ts-ignore
-
-            push(`/profile?id=${ window.event.currentTarget.getAttribute('data-id') }`)
+            push(`/profile?id=${window.event.currentTarget.getAttribute('data-id')}`)
         }
-    },[])
+    }, [])
 
 
-    if(!config.height) {
+    if (!config.height) {
         return <div>loading...</div>
     }
     return <article className="views-story">
-        <div className="close iconfont iconclose" onClick={() => { goBack() }}/>
-        <AwesomeSwiper config={ config  }   className="swiper-container">
-            <div className="swiper-wrapper stories">
-              {/*  {
-                    list.map((i,index) => {
-                        return    <div className="swiper-slide story"
-                                       key={ index }
-                            >
-                            <div className="story-bg">
-                                <img src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1608067339,398987776&fm=26&gp=0.jpg" alt=""/>
-                            </div>
-                            <div className="story-content">
-                                <div className="user-wrapper">
-                                    <div className="avatar-wrapper">
-                                        <img src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3728388047,2037988101&fm=26&gp=0.jpg" alt="" className="avatar"/>
-                                    </div>
-                                    <div className="base">
-                                        <h1 className="name line-1">Jasmine Levin</h1>
-                                        <p className="date">4m ago</p>
-                                    </div>
-                                </div>
-                                <div className="hot">
-                                    <h1 className="title">
-                                        Do You Want To Live A Happy Life? Smile.
-                                    </h1>
-                                    <div className="content">
-                                        Sometimes there’s no reason to smile, but I’ll smile anyway because of
-                                        life. Yes, I’m one of those people who always smiles.
-                                    </div>
-                                </div>
-                                <div className="heart-wrapper">
-                                    <span className="heart iconfont iconheart active"></span>
-                                    <span className="digital">450K</span>
-                                </div>
-                            </div>
-                        </div>
-                    })
-                }*/}
-            </div>
+        <div className="close iconfont iconclose" onClick={() => {
+            goBack()
+        }}/>
+        <AwesomeSwiper config={config} className="swiper-container">
+            <div className="swiper-wrapper stories"/>
         </AwesomeSwiper>
+        <Dynamic/>
     </article>
 })
 export default Story
